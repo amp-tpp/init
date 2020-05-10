fc0c20c3febc832f23db6cafa4ba8bc2e9c4b014
Pákozdi György <gyorgypakozdi@gmail.com>
2020-05-10 12:03:59 +0200

    Update dev-config.sh

--- dev-config.sh
+++ dev-config.sh
@@ -2,9 +2,10 @@ echo Clean psi enviroment
 /bin/rm -rf *
 read -p 'GH Username: ' uservar
 read -sp 'GH Password: ' passvar
+echo
 {
   echo Clone psi core
-  git clone https://$uservar:$passvar@github.com/transparent-psi/transparent-psi.git > /dev/null
+  git clone --quiet https://$uservar:$passvar@github.com/transparent-psi/transparent-psi.git > /dev/null
 } || {
   echo Auth error
   exit $1
@@ -16,3 +17,15 @@ echo Clone psi images
 git clone --quiet https://$uservar:$passvar@github.com/transparent-psi/transparent-psi-images.git > /dev/null
 echo Clone psi languages
 git clone --quiet https://$uservar:$passvar@github.com/transparent-psi/transparent-psi-languages.git > /dev/null
+echo
+echo Install core/client
+cd transparent-psi/client && npm i --silent > /dev/null && cd ../../
+echo Install core/backend
+cd transparent-psi/backend && npm i --silent > /dev/null && cd ../../
+echo Install core/admin
+cd transparent-psi/admin && npm i --silent > /dev/null && cd ../../
+echo Install images/demo
+cd transparent-psi-images/demo && npm i --silent > /dev/null && cd ../../
+echo Install images/production
+cd transparent-psi-images/production && npm i --silent > /dev/null && cd ../../
+echo Happy hacking!
