7538c961b22576fb5dc9a75d5454f36f1091ea10
Pákozdi György <gyorgypakozdi@gmail.com>
2020-05-10 11:34:45 +0200

    Update dev-config.sh

--- dev-config.sh
+++ dev-config.sh
@@ -2,8 +2,15 @@ read -p 'GH Username: ' uservar
 read -sp 'GH Password: ' passvar
 {
   echo Clone psi core
-  git clone https://$uservar:$passvar@github.com/transparent-psi/transparent-psi.git
+  git clone https://$uservar:$passvar@github.com/transparent-psi/transparent-psi.git > /dev/null
 } || {
   echo Auth error
   exit $1
 }
+
+echo Clone psi results
+git clone https://$uservar:$passvar@github.com/transparent-psi/transparent-psi-results.git > /dev/null
+echo Clone psi images
+git clone https://$uservar:$passvar@github.com/transparent-psi/transparent-psi-images.git > /dev/null
+echo Clone psi languages
+git clone https://$uservar:$passvar@github.com/transparent-psi/transparent-psi-languages.git > /dev/null
